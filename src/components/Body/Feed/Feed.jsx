9eb78d64b1da4b2a8e1c4175d19a216e7328b8c6
shivprajapat <shivkumarprajapat232@gmail.com>
2022-07-05 11:38:39 +0530

    firebase post functionality Added

--- src/components/Body/Feed/Feed.jsx
+++ src/components/Body/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import "./feed.css";
 import { InputOption, Post } from "../index";
 import EditIcon from "@mui/icons-material/Edit";
@@ -6,12 +6,31 @@ import ImageIcon from "@mui/icons-material/Image";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import EventNoteIcon from "@mui/icons-material/EventNote";
+import { db } from '../../../firebase';
+import firebase from 'firebase';
 const Feed = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
+  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    db.collection("posts").orderBy('timestamp', 'desc').onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          data: doc.data(),
+        }
+      )))
+    ))
+  }, [])
   const sendPost = (e) => {
     e.preventDefault();
+    db.collection("posts").add({
+      name: "shiv",
+      description: "this is a new description",
+      message: input,
+      photoUrl:  '',
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
   };
   return (
     <div className="feed">
@@ -19,14 +38,8 @@ const Feed = () => {
         <div className="feed_input">
           <EditIcon />
           <form>
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              type="text"
-            />
-            <button onClick={sendPost} type="submit">
-              Send
-            </button>
+            <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed_Option">
@@ -41,7 +54,15 @@ const Feed = () => {
         </div>
       </div>
       {/* Post */}
-      <Post name="shiv" description="this is test comment" message="Wow" />
+      {posts.map(({ id, data: { name, description, message, photoUrl}}) => (
+          <Post 
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />
+        ))}
       {/* ./Post */}
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDwQRKns7wlmbFYOtzKM6SDZbc14ZyCBB8",
     authDomain: "linkedin-clone-3f85e.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
     messagingSenderId: "33975971781",
     appId: "1:33975971781:web:a8775ec9174a5838fde5ac"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  
+  export { db, auth };  
