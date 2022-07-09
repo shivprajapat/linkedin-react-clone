8439e844bad601eda1e800bd05d9514791e8d89a
shivprajapat <shivkumarprajapat232@gmail.com>
2022-07-09 11:30:31 +0530

    register form functionality added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import { Feed, Sidebar } from "./components/Body";
 import Header from "./components/Header/Header";
 import { Container } from "@mui/material";
 import Login from "./components/Login";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      <Login/>
-      {/* <Container maxWidth="lg">
-        <div className="app_body">
-          <Sidebar />
-          <Feed />
-        </div>
-      </Container> */}
+
+      <Container maxWidth="lg">
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app_body">
+            <Sidebar />
+            <Feed />
+          </div>
+        )}
+      </Container>
     </div>
   );
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,33 +1,82 @@
-import React from "react";
-import './style.css'
-import logoBig from '../../Images/logo-big.png'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
+import { auth } from "../../firebase";
+import "./style.css";
+import logoBig from "../../Images/logo-big.png";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  profilePic: "",
+};
+
 const Login = () => {
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
   const loginToApp = (e) => {
     e.preventDefault();
   };
 
-  const register = () => {};
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profilePic,
+              })
+            );
+          });
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
       <img src={logoBig} alt="Linkedin logo" />
-
       <form>
         <input
           type="text"
-           placeholder="Full name (required if registering)"
+          placeholder="Full name (required if registering)"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           type="text"
           placeholder="Profile picture URL (optional)"
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
         />
         <input
           type="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit" onClick={loginToApp}>
           Sign In
